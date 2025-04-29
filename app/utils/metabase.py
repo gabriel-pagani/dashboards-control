5e5d6d5bfb80a8c6ce0e6791afbac280f72f452a
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-04-29 13:54:24 -0300

    atualizei o template 'index.html' para incluir um iframe com o dashboard e criei uma função para gerar o endereço do dashboard

---
+++ app/utils/metabase.py
@@ -0,0 +1,21 @@
+import jwt
+import time
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+
+def generate_dashboard_url(dashboard_id):
+    payload = {
+        "resource": {"dashboard": dashboard_id},
+        "params": {},
+        "exp": round(time.time()) + (60 * 10)
+    }
+
+    token = jwt.encode(payload, os.getenv(
+        'METABASE_SECRET_KEY'), algorithm="HS256")
+
+    url = f"{os.getenv('METABASE_SITE_URL')}/embed/dashboard/{token}#bordered=true&titled=true"
+
+    return url
--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utils.metabase import generate_dashboard_url
 
 
 def home(request):
     return render(
         request,
         'index.html',
+        {'dashboard': generate_dashboard_url(38)}
     )
 
 
