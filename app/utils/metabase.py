1cb716c0a385cd1b1372dcbe47becf78b9886586
Gabriel Souza <gabriel.souza@sinasc.com.br>
2025-05-02 09:20:55 -0300

    Adicionei cache para não gerar varias urls antes do tempo de expiração

--- app/utils/metabase.py
+++ app/utils/metabase.py
@@ -4,9 +4,15 @@ import os
 import dotenv
 
 dotenv.load_dotenv()
+token_cache = {}
 
 
 def generate_dashboard_url(dashboard_id):
+    current_time = time.time()
+    cached = token_cache.get(dashboard_id)
+    if cached and current_time < cached['exp']:
+        return cached['url']  # Retorna URL existente se ainda estiver válida
+
     payload = {
         "resource": {"dashboard": dashboard_id},
         "params": {},
@@ -17,5 +23,11 @@ def generate_dashboard_url(dashboard_id):
         'METABASE_SECRET_KEY'), algorithm="HS256")
 
     url = f"{os.getenv('METABASE_SITE_URL')}/embed/dashboard/{token}#bordered=true&titled=true"
+
+    # Atualiza o cache
+    token_cache[dashboard_id] = {
+        "url": url,
+        "exp": round(time.time()) + (60 * 5)
+    }
 
     return url
