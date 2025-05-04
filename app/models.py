e81594d0c36b744bc8d1ce9fb3930049a45a7e5c
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2025-05-04 15:49:55 -0300

    Limitei os valores para o setor

--- app/models.py
+++ app/models.py
@@ -3,11 +3,21 @@ from django.contrib.auth.models import User, Group
 
 
 class Dashboard(models.Model):
+    SETORES = [
+        ('Tecnologia da Informação', 'Tecnologia da Informação'),
+        ('Recursos Humanos', 'Recursos Humanos'),
+        ('Financeiro', 'Financeiro'),
+        ('Administrativo', 'Administrativo'),
+    ]
+
     titulo = models.CharField(max_length=100)
-    setor = models.CharField(max_length=100)
+    setor = models.CharField(max_length=100, choices=SETORES)
     codigo = models.IntegerField()
     usuarios = models.ManyToManyField(User, related_name='dashboards', blank=True)
     grupos = models.ManyToManyField(Group, related_name='dashboards', blank=True)
 
     def __str__(self) -> str:
         return self.titulo
+
+    class Meta:
+        ordering = ['setor']
